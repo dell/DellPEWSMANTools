0d20024ac1c84143df4043f6dfbd74951638d155
Ravikanth C <ravikanth@ravichaganti.com>
2017-03-24 19:55:04 +0530

    Updates to Enclosure metadata

--- PEOSD/PEOSD.psm1
+++ PEOSD/PEOSD.psm1
@@ -16,13 +16,50 @@
     }
 
     Process {
-        foreach ($session in $iDRACSession) {
-            $result = Invoke-CimMethod -InputObject $instance -MethodName GetDriverPackInfo -CimSession $iDRacsession
-            if ($result.ReturnValue -ne 0) {
-                Write-Error $result.Message
-            } else {
-                $result
-            }
+        $result = Invoke-CimMethod -InputObject $instance -MethodName GetDriverPackInfo -CimSession $iDRACSession
+        if ($result.ReturnValue -ne 0) {
+            Write-Error $result.Message
+        } else {
+            $result
+        }
+    }
+}
+
+Function Attach-PEDriverPack {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false
+        )]
+        [Alias("s")]
+        $iDRACSession,
+
+        [Parameter(Mandatory)]
+        [String] $OSName,
+
+        [Parameter()]
+        [int] $Duration
+    )
+
+    Begin {
+        $properties= @{SystemCreationClassName="DCIM_ComputerSystem";SystemName="DCIM:ComputerSystem";CreationClassName="DCIM_OSDeploymentService";Name="DCIM:OSDeploymentService";}
+        $instance = New-CimInstance -ClassName DCIM_OSDeploymentService -Namespace root/dcim -ClientOnly -Key @($properties.keys) -Property $properties
+    }
+
+    Process {
+        $argumentHash = @{'OSName'="$OSName"}
+        if ($Duration)
+        {
+            $argumentHash.Add('Duration',$Duration)
+        }
+
+        $result = Invoke-CimMethod -InputObject $instance -MethodName UnpackAndAttach -Arguments $argumentHash -CimSession $iDRACSession
+        if ($result.ReturnValue -ne 0) {
+            Write-Error $result.Message
+        } else {
+            $result
         }
     }
 }
