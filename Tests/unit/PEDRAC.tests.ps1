c92d44391553d0026673af6d988dcf796f0a4baa
DexterPOSH <dexter34925@yahoo.in>
2017-07-04 11:39:04 +0530

    Adds unit tests for Get-PEDRACInformation

--- Tests/unit/PEDRAC.tests.ps1
+++ Tests/unit/PEDRAC.tests.ps1
@@ -86,3 +86,56 @@ Describe 'New-PEDRACSession' -Tag UnitTest {
         }
     }
 }
+
+Describe 'Get-PEDRACInformation' {
+    
+    Context "Querying the class is successful" {
+        # Arrange
+        Mock -Command Get-CimInstance -MockWith {
+            [pscustomobject]@{
+                'iDRAC'=$true
+            }
+        } -ParameterFilter {
+            # param filters here
+            ($cimsession -ne $null) -and
+            ($ClassName-eq 'DCIM_iDRACCardView') -and
+            ($NameSpace -eq 'root\dcim')
+        } -Verifiable
+
+        # Act
+        $Output = Get-PEDRACInformation -iDRACSession 'dummy'
+
+        # Assert
+        It "Should query the correct classname in correct namespace" {
+            Assert-MockCalled -CommandName Get-CimInstance -Times 1 -Exactly -Scope Context
+            Assert-VerifiableMocks # assert that our mock was called
+        }
+
+        It "Should return the mocked output" {
+            $Output | Should NOT BeNullOrEmpty
+            $Output.iDRAC | Should Be $True
+        }
+
+
+    }
+
+    Context "Querying the class failed" {
+        Mock -CommandName Get-CimInstance -MockWith { throw 'failure'} -ParameterFilter {
+            ($cimsession -ne $null) -and
+            ($ClassName -eq 'DCIM_iDRACCardView') -and
+            ($NameSpace -eq 'root\dcim')
+        } -Verifiable
+
+        $Output = Get-PEDRACInformation -iDRACSession 'dummy' -ErrorVariable PEDRACInfoError
+
+        It "Should write the error to the error stream" {
+            $PEDRACInfoError | Should NOT BeNullOrEmpty
+            $Output | Should BeNullOrEmpty
+        }
+
+        It "Should query the correct class and namespace" {
+            Assert-VerifiableMocks
+        }
+    }
+
+}
